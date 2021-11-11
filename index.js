eddd8e4568aec69ebd2bc4ef9bcde8fe3485be88
Paula Dettmann <paula.dettmann@gmail.com>
2021-11-11 14:18:54 +0100

    add query for brunch, bars and restaurants

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// setting up app server - express
-// connection to db collection - mongodb
-// query list from db
-// send list to frontend
 // query user with password and email
 // send user details (profile)
+
+const db = require('./db');
 
 var express = require("express");
 var app = express();
@@ -12,8 +10,24 @@ app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-   });
+app.get("/bars", async (req, res, next) => {
+    const query = {category: "Best Bars"};
+    const sorting = {rating: -1};
+    const result = await db(query, sorting);
+    res.json(result);
+});
 
+app.get("/restaurants", async (req, res, next) => {
+    const query = {category: "Best Restaurants"};
+    const sorting = {rating: -1};
+    const result = await db(query, sorting);
+    res.json(result);
+});
+
+app.get("/brunch", async (req, res, next) => {
+    const query = {category: "Brunch"};
+    const sorting = {rating: -1};
+    const result = await db(query, sorting);
+    res.json(result);
+});
 
